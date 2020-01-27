a32a457bf9bb8cdaa83e76b469f8b90df3e10dc1
MB <mbilal_ce@live.com>
2020-01-27 21:09:47 +0000

    Parallelized benchmark

--- algorithms/arena.py
+++ algorithms/arena.py
@@ -10,20 +10,63 @@ from simulatedannealing import saOpt
 import json
 import os
 import time
+from joblib import Parallel, delayed
 
-def getResult(search, filename, dir='logs/', log=True):
-    results = search.runOptimizer()
-    data =dict()
-    # print(results)
-    if os.path.isfile(dir+filename):
-        data = json.load(open(dir+filename, 'r'))
+
+def getResults(search, filename, config, dir='logs/'):
+    log = config['log']
+    for i in range(0, config["num_of_runs"]):
+        results = search.runOptimizer()
+        data =dict()
+        # print(results)
+        if os.path.isfile(dir+filename):
+            data = json.load(open(dir+filename, 'r'))
+        else:
+            data['experiments'] = []
+        # print(results)
+        data['experiments'].append(results['trials'])
+
+        if log:
+            json.dump(data, open(dir+filename, 'w'), indent=4)
+
+def callBO(system, app, datasize, algo, budget, parent_dir, types, sizes, number_of_nodes, config, estimator, acq_method, filename):
+    new_filename = filename + '_' + estimator + '_' + acq_method
+    print(new_filename)
+    search = boSkOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, optimizer=estimator, 
+                        initial_samples=config["initial_samples"], acquisition_method=acq_method)
+    getResults(search, new_filename, config)
+
+
+def callOptimizer(system, app, datasize, algo, budget, parent_dir, types, sizes, number_of_nodes, config):
+    filename = system + '_' + app + '_' + datasize + '_' + algo
+    if algo == "lhs":
+        search = lhsSearch(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
+    elif algo == "random":
+        search = randSearch(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
+    elif algo == "random2x":
+        search = randSearch(app, system, datasize, 2*budget, parent_dir, types, sizes, number_of_nodes)
+    elif algo == "smac":
+        search = smac(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
+    elif algo == "tpe":
+        search = tpeOptimizer(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
+
+    elif algo == "bo":
+        Parallel(n_jobs=nJobs)(delayed(callBO)(system, app, datasize, algo, budget, parent_dir, types, sizes, number_of_nodes, config, estimator, acq_method, filename )
+                    for estimator in config["bo_estimators"] for acq_method in config["bo_acq"][estimator])
+
+
+    elif "hc" in algo:
+        search = hcOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
+    elif "sa" in algo:
+        search = saOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
+
     else:
-        data['experiments'] = []
-    # print(results)
-    data['experiments'].append(results['trials'])
+        print("Algorithm not found")
 
-    if log:
-        json.dump(data, open(dir+filename, 'w'), indent=4)
+
+    if algo != "bo":
+        getResults(search, filename, config)
+
 
 number_of_nodes = {
 'large': [4, 6, 8, 10, 12, 16, 24, 32, 40, 48],
@@ -38,44 +81,15 @@ parent_dir = '../scout/dataset/osr_multiple_nodes/'
 config = json.load(open(sys.argv[1], 'r'))
 
 budget = config["budget"]
+nJobs = 4
 
 for system in config["systems"]:
     for app in config["applications"][system]:
         for datasize in config["datasizes"]:
             for algo in config["bbo_algos"]:
-                for i in range(0, config["num_of_runs"]):
-                    filename = system + '_' + app + '_' + datasize + '_' + algo
-                    if algo == "lhs":
-                        search = lhsSearch(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
-                    elif algo == "random":
-                        search = randSearch(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
-                    elif algo == "random2x":
-                        search = randSearch(app, system, datasize, 2*budget, parent_dir, types, sizes, number_of_nodes)
-                    elif algo == "smac":
-                        search = smac(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes)
-                    elif algo == "tpe":
-                        search = tpeOptimizer(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
+                callOptimizer(system, app, datasize, algo, budget, parent_dir, types, sizes, number_of_nodes, config) 
+                                                
 
-                    elif algo == "bo":
-                        for estimator in config["bo_estimators"]:
-                            for acq_method in config["bo_acq"][estimator]:
-                                new_filename = filename + '_' + estimator + '_' + acq_method
-                                print(new_filename)
-                                search = boSkOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, optimizer=estimator, 
-                                                    initial_samples=config["initial_samples"], acquisition_method=acq_method)
-                                getResult(search, new_filename, log=config['log'])
-
-                    elif "hc" in algo:
-                        search = hcOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
-                    elif "sa" in algo:
-                        search = saOpt(app, system, datasize, budget, parent_dir, types, sizes, number_of_nodes, initial_samples=config["initial_samples"])
-                
-                    else:
-                        print("Algorithm not found")
-
-
-                    if algo != "bo":
-                        getResult(search, filename, log=config['log'])
 
 
 
